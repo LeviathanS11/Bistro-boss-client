b2c140d523f6fd8c693359c9affeb1b7654f5a13
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-31 04:02:30 +0600

    captcha added,firebase config added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+
 const Login = () => {
+    const [disable,setDisable]=useState(true);
+    //useRef diye input field er value read kora holo.jei input field er value read korte chai sheikhane ref={captchaRef} add korte hobe.
+    const captchaRef=useRef(null)
+    const handleValidateCaptcha=()=>{
+        const value=captchaRef.current.value;
+        if(validateCaptcha(value)){
+            setDisable(false)
+        }
+        else{
+            setDisable(true)
+        }
+    }
+
+    useEffect(()=>{
+        loadCaptchaEnginge(6);
+    },[])
+
+    const handeLogin=event=>{
+        event.preventDefault();
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log(email,password)
+    }
     return (
-        <div>
-            <h2>Please Login...</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col md:flex-row-reverse">
+                <div className="text-center md:w-1/2 lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card md:w-1/2  max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handeLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                            <LoadCanvasTemplate />
+                            </label>
+                            <input type="text" ref={captchaRef} name="captcha" placeholder="type the text aboves" className="input input-bordered" />
+                            <button onClick={handleValidateCaptcha} className='btn btn-outline btn-xs'>Validate</button>
+                        </div>
+                        <div className="form-control mt-6">
+                            
+                            <input disabled={disable} className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
